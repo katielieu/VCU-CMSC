0f38ac7f8a060137f36c0088afb54e2a3bae2752
katielieu <164425785+katielieu@users.noreply.github.com>
2024-05-19 01:11:44 -0400

    Add files via upload

---
+++ 255 Intro to Programming/Labs/Lab 2/Test5.java
@@ -0,0 +1,31 @@
+public class Ducky
+{
+  public static void main(String[] args)
+  {
+    System.out.println("   ");
+    System.out.println("    ****");
+    System.out.println("  **  **");
+    System.out.println("********");
+    System.out.println("    ****          **");
+    System.out.println("    ******        **");
+    System.out.println("    ****************");
+    System.out.println("    ****************");
+    System.out.println("    **************");
+    System.out.println("      **********");
+    System.out.println("      **********");
+    System.out.println("        ******");
+    System.out.println();
+    System.out.println("This might be a duck.\n");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ 255 Intro to Programming/Labs/Lab 2/Triangle.java
@@ -0,0 +1,19 @@
+/*
+Triangle.java
+The purpose of this program is to display a triangle
+
+Katie Lieu
+1/24/24
+CMSC 255 Section 4 
+*/
+
+public class Triangle {
+	public static void main(String args[]){
+		System.out.println("    /\\");
+		System.out.println("   /  \\");
+		System.out.println("  /    \\");
+		System.out.println(" /      \\");
+		System.out.println("/        \\");
+		System.out.println("__________");
+	}
+}
