37317c0d9e0da01e407b7e46b0f255ce2d062d54
katielieu <164425785+katielieu@users.noreply.github.com>
2024-05-19 01:15:40 -0400

    Add files via upload

---
+++ 255 Intro to Programming/Projects/Project 1/NameArt.java
@@ -0,0 +1,35 @@
+/*
+NameArt
+This project outputs my name in a unique way on the display screen
+ 
+ Katie Lieu
+ 1/22/24
+ CMSC 255 Section 4
+ */
+public class NameArt {
+	public static void main(String[] args) {
+		// Information identifying output statements
+		System.out.println("Katie Lieu");
+		System.out.println("Project 1 -- Name in Code");
+		System.out.println("This project outputs my name in a unique way on the display screen");
+		System.out.println("January 22, 2024");
+		System.out.println("CMSC 255 Section 4");
+
+		// Write my name in cursive calligraphy with cat details around it
+		System.out.println("");
+		System.out.println("    / \\                                                                      /\\");
+		System.out.println("   /   \\      **         *                      **                          /  \\");
+		System.out.println("  /     \\     **        *                       **                         /    \\");
+		System.out.println(" /  /\\   \\    **       *                        **                        /  /\\  \\");
+		System.out.println("/  /  \\   \\   **      *                         **                       /  /  \\  \\");
+		System.out.println("-----------   **     *                          **                       -----------");
+		System.out.println("              **    *                         ******    **");
+		System.out.println("  *           ** ***              *****  **     **                 ***            *");
+		System.out.println("    ***       **    **          **      ***     **      **       **    *      ***");
+		System.out.println("        ***   **     **	        **      ***     **      **       **    *   ***");
+		System.out.println(" ***********  **      **        **       **     **      **       ******   ***********");
+		System.out.println("        ***   **        **     ***      ***    ***     ***      ***        ***");
+		System.out.println("    ***       **         **   *  **     * **  * **  **   **    * **    **     ***");
+		System.out.println("  *           **          ***     *****    ***   ***       ****    ****           *");
+	}
+}
