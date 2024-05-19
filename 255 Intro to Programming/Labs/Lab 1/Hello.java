e9f3c07f5c838123ae662ff5b78fa7bef69363a7
katielieu <164425785+katielieu@users.noreply.github.com>
2024-05-19 01:07:43 -0400

    Add files via upload

---
+++ 255 Intro to Programming/Labs/Lab 1/Hello.java
@@ -0,0 +1,18 @@
+/*
+*Hello.java
+*
+*Katie Lieu
+*1/17/2024
+*CMSC 255 Section 004
+*/
+
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("Katie Lieu");
+		System.out.println("Hello.java");
+		System.out.println("Hello / Installing Java");
+		System.out.println("1/17/2024");
+		System.out.println("CMSC 255 Section 004");
+		System.out.println();
+	}
+}
